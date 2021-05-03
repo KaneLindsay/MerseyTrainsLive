0a0a81ac9420f2018cb29b5703d4d1bcabf65b09
Isaac Ward <Isaac.ward@virgin.net>
2021-05-03 13:49:57 +0100

    API stuff mostly
    Consumer.java handles train movements API, ScheduleManager handles schedule JSON files, service is train, station is station (may need to edit current one), TrainManager handles train data.

---
+++ app/src/main/java/com/example/Isaac stuff/Service.java
@@ -0,0 +1,43 @@
+public class Service {
+    private String serviceCode;
+    private String trainID;
+    private String dateCreated;
+    private String currentLocation;
+    private int delay;
+    private boolean atStation;
+
+    public Service(String[] activationMessage ) {   //Constructor is given the activationMessage from the API
+        serviceCode = activationMessage[0];
+        trainID = activationMessage[1];
+        dateCreated = activationMessage[2];
+        currentLocation = activationMessage[3];
+        atStation = false;
+    }
+
+    public void trainMovement(String[] movementMessage) {   //Method is given the movementMessage from the API
+        if (movementMessage[2] == "ARRIVAL") {
+            currentLocation = movementMessage[4];
+            atStation = true;
+        }  else {// if (movementMessage[2] == "DEPARTURE")
+            atStation = false;
+        }
+    }
+
+    //Find station linked to STANOX, place train in the station or between this and the next station, depending on atStation
+
+    //Getters for private variables
+    public String getDateCreated() {
+        return dateCreated;
+    }
+    public String getCurrentLocation() {
+        return currentLocation;
+    }
+    public String getServiceCode() {
+        return serviceCode;
+    }
+    public String getTrainID() {
+        return trainID;
+    }
+
+    //NEED PROCESSING OF TRAIN MOVEMENTS, CANCELLATION WILL BE HANDLED BY THE MANAGER
+}
---
+++ app/src/main/java/com/example/Isaac stuff/Station.java
@@ -0,0 +1,15 @@
+public class Station {
+    private String stationName;
+    private String stanox;
+    private int platforms;
+    private String tiploc;
+
+    public Station(String s, String stan, int plat, String tip) {
+        stationName = s;
+        stanox = stan;
+        platforms = plat;
+        tiploc = tip;
+    }
+
+
+}
