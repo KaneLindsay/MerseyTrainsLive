b60935758aded2511723695f48edfabe2d7b2c08
Isaac Ward <Isaac.ward@virgin.net>
2021-05-03 21:06:36 +0100

    Added example for station definition
    <3 dylan

--- app/src/main/java/com/example/merseytrainslive/livedata/Station.java
+++ app/src/main/java/com/example/merseytrainslive/livedata/Station.java
@@ -1,4 +1,6 @@
-package com.example.merseytrainslive.livedata;
+-package com.example.mersrytrainslive.livedata;
+import java.util.Arrays;
+import java.util.List;
 
 public class Station {
     private String stationName;
@@ -6,12 +8,22 @@ public class Station {
     private int platforms;
     private String tiploc;
 
+
+    //Constructor
     public Station(String s, String stan, int plat, String tip) {
         stationName = s;
         stanox = stan;
         platforms = plat;
         tiploc = tip;
+
+        //Example station definition:
+        Station lol = new Station("Liverpool Lime Street", "stanox", 8, "tiploc");
     }
 
-
+    //getters for private variables
+    public String getStanox() { return stanox; }
+    public String getStationName() { return stationName; }
+    public String getTipLoc() { return tiploc; }
 }
+
+
